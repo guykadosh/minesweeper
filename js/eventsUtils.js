a03f321350f7c89ea414747eec746515a2c4c155
Guy Kadosh <99971417+guykadosh@users.noreply.github.com>
2022-05-26 21:58:18 +0300

    FIXED UPLOAD

---
+++ js/eventsUtils.js
@@ -0,0 +1,177 @@
+///////////////////////////
+/// Handle Events Utils///
+/////////////////////////
+
+// Timers
+
+// Initialize and render timer
+function startTimer() {
+  // Clear if there is previous interval
+  clearInterval(gInterval);
+
+  gInterval = setInterval(function () {
+    var timeDiffMs = Date.now() - gStartTime;
+    // Update DOM
+    var elTimer = document.querySelector('.timer');
+    elTimer.innerText = convertMsToTime(timeDiffMs);
+  }, 59);
+}
+
+// Check end time
+function getEndtime() {
+  gEndTime = new Date();
+  var timeDiff = gEndTime - gStartTime; //in ms
+  // strip the ms
+  return timeDiff;
+}
+
+// Once called when game done checks bext time and render it
+function checkBestTime() {
+  var time = getEndtime();
+
+  switch (gLevel.SIZE) {
+    case 4:
+      if (!gBestTimeBeginner) localStorage.setItem('bestTimeBeginner', time);
+      if (gBestTimeBeginner > time)
+        localStorage.setItem('bestTimeBeginner', time);
+      gBestTimeBeginner = +localStorage.getItem('bestTimeBeginner');
+      renderBestTime();
+      break;
+    case 8:
+      if (!gBestTimeMedium) localStorage.setItem('bestTimeMedium', time);
+      if (gBestTimeMedium > time) localStorage.setItem('bestTimeMedium', time);
+      gBestTimeMedium = localStorage.getItem('bestTimeMedium');
+      renderBestTime();
+      break;
+    case 12:
+      if (!gBestTimeExpert) localStorage.setItem('bestTimeExpert', time);
+      if (gBestTimeExpert > time) localStorage.setItem('bestTimeExpert', time);
+      gBestTimeExpert = localStorage.getItem('bestTimeExpert');
+      renderBestTime();
+      break;
+  }
+}
+
+// End Game
+
+// Handle game lost state
+function gameLost() {
+  // Model
+  gGame.isOver = true;
+  gGame.isOn = false;
+
+  clearInterval(gInterval);
+
+  // reveals all the board
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[0].length; j++) {
+      if (gBoard[i][j].isShown) continue;
+
+      // Model
+      gBoard[i][j].isShown = true;
+
+      // DOM
+      showCellByLoc({ i, j });
+    }
+  }
+
+  // DOM
+  renderGameState(DEAD_ICON);
+  renderModeTitle('You Got Bombed!');
+}
+
+// Handle game won state
+function gameWon() {
+  clearInterval(gInterval);
+
+  // Model
+  gGame.isOver = true;
+  gGame.isOn = false;
+
+  // DOM
+  renderGameState(WON_ICON);
+  renderModeTitle('Well Done!');
+
+  // Put flags on all bombs not marked and reveals rest of board
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[0].length; j++) {
+      if (gBoard[i][j].isShown) continue;
+
+      if (gBoard[i][j].isMine) {
+        // Model
+        gBoard[i][j].isMarked = true;
+        gGame.markedCount++;
+        // DOM
+        renderMarkByLoc({ i, j });
+      } else {
+        // Model
+        gBoard[i][j].isShown = true;
+        // DOM
+        showCellByLoc({ i, j });
+      }
+    }
+  }
+
+  // Check if best score
+  checkBestTime();
+}
+
+// Checks if user won the game
+function checkGameOver() {
+  return checkNonMinesShown() || checkMinesFlagged();
+}
+
+// first win condition - all non mines are revealed
+function checkNonMinesShown() {
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[0].length; j++) {
+      var curCell = gBoard[i][j];
+      if (!curCell.isShown && !curCell.isMine) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// second win condition - if not all revealed but all mines are flagged
+function checkMinesFlagged() {
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[0].length; j++) {
+      var curCell = gBoard[i][j];
+      if (curCell.isMine && !curCell.isMarked) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// Expand  shown cells for every cell that had no mine neighbors
+function expandShown(board, rowIdx, colIdx) {
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= board.length) continue;
+
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= board[i].length) continue;
+      if (i === rowIdx && j === colIdx) continue;
+
+      var currCell = board[i][j];
+
+      if (currCell.isShown) continue;
+
+      // Model
+      currCell.isShown = true;
+      gGame.shownCount++;
+      // DOM
+      showCellByLoc({ i, j });
+
+      // if finds another cell with no neighbors render its surrounding aswell
+      if (currCell.minesAroundCount === 0) expandShown(board, i, j);
+      gGame.moves[gGame.moves.length - 1].push({
+        cell: currCell,
+        location: { i, j },
+      });
+    }
+  }
+}
---
+++ js/utils.js
@@ -0,0 +1,86 @@
+'use strict';
+
+//////////////////////////////
+/// Model & general Utils ///
+////////////////////////////
+
+// Create Mat in given size(a square if only 1 size)
+function createMat(ROWS, COLS = ROWS) {
+  var mat = [];
+  for (var i = 0; i < ROWS; i++) {
+    var row = [];
+    for (var j = 0; j < COLS; j++) {
+      row.push('');
+    }
+    mat.push(row);
+  }
+  return mat;
+}
+
+// Counts neighbouring mines
+function getMinesAroundCounts(board, rowIdx, colIdx) {
+  var count = 0;
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= board.length) continue;
+
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= board[i].length) continue;
+      if (i === rowIdx && j === colIdx) continue;
+
+      var currCell = board[i][j];
+      if (currCell.isMine) count++;
+    }
+  }
+
+  return count;
+}
+
+function getIdName(location) {
+  var cellId = 'cell-' + location.i + '-' + location.j;
+  return cellId;
+}
+
+function getCellCoord(strCellId) {
+  var parts = strCellId.split('-');
+  var coord = { i: +parts[1], j: +parts[2] };
+  return coord;
+}
+
+// Finding all the safe cells and returns a random safe cell's location'
+function getSafeCellLoc() {
+  const safeCells = [];
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[0].length; j++) {
+      if (!gBoard[i][j].isMine) safeCells.push({ i, j });
+    }
+  }
+
+  return safeCells[getRandomIntInc(0, safeCells.length - 1)];
+}
+
+// Padding '0' to a lonely digit.
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+// Converts milliseconds to a readable String
+function convertMsToTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+
+  return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}:${(
+    milliseconds % 1000
+  )
+    .toString()
+    .substring(0, 3)
+    .padStart(3, '0')}`;
+}
+
+function getRandomIntInc(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
